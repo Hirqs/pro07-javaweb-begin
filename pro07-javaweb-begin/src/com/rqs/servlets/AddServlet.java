a6082fc6d1e63b7c24c05e2998365441e31f3a0b
Hirqs <569543446@qq.com>
2022-03-15 23:13:53 +0800

    Initial commit

---
+++ pro07-javaweb-begin/src/com/rqs/servlets/AddServlet.java
@@ -0,0 +1,33 @@
+package com.rqs.servlets;
+
+
+import com.rqs.fruit.dao.FruitDAO;
+import com.rqs.fruit.dao.impl.FruitDAOImpl;
+import com.rqs.fruit.pojo.Fruit;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String fname = request.getParameter("fname");
+        String priceStr = request.getParameter("price");
+        Integer price = Integer.parseInt(priceStr);
+        String fcountStr = request.getParameter("fcount");
+        Integer fcount = Integer.parseInt(fcountStr);
+        String remark = request.getParameter("remark");
+
+        System.out.println("哈哈");
+
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        boolean flag = fruitDAO.addFruit(new Fruit(0 , fname , price , fcount , remark));
+
+        System.out.println(flag ? "添加成功！" : "添加失败！");
+    }
+}
